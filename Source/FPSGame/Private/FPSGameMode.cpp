3b1760abd4357b5c2dce3801266b6acbc414a98f
joshua3403 <joshua3403@gmail.com>
2019-10-07 10:33:26 +0900

    08. Setup Post-Mission Camera
    Add new Camera view point by blueprint and C++ class

--- Source/FPSGame/Private/FPSGameMode.cpp
+++ Source/FPSGame/Private/FPSGameMode.cpp
@@ -4,6 +4,7 @@
 #include "FPSHUD.h"
 #include "FPSCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
 
 AFPSGameMode::AFPSGameMode()
 {
@@ -20,7 +21,30 @@ void AFPSGameMode::CompleteMission(APawn* InstigatorPawn)
 	if (InstigatorPawn)
 	{
 		InstigatorPawn->DisableInput(nullptr);
+
+		if (SpectatingViewpointClass)
+		{
+			TArray<AActor*> ReturnActors;
+			UGameplayStatics::GetAllActorsOfClass(this, SpectatingViewpointClass, ReturnActors);
+
+			// Change viewtarget if any valid actor found
+			if (ReturnActors.Num() > 0)
+			{
+				AActor* NewViewTarget = ReturnActors[0];
+
+				APlayerController* PC = Cast<APlayerController>(InstigatorPawn->GetController());
+				if (PC)
+				{
+					PC->SetViewTargetWithBlend(NewViewTarget, 0.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+				}
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Log, TEXT("SpectatingViewpointClass is nullptr. Please update GameMode class with valid subclass. Cannot change spectating viewe targets."));
+		}
 	}
 
 	OnMissionCompleted(InstigatorPawn);
+
 }
